2658bc54d4c8d681da6733f250203907761035d3
Erich Kauffman <kauffm2@stolaf.edu>
2017-12-06 08:20:23 -0600

    Add output to file

--- examples/traverseNode.py
+++ examples/traverseNode.py
@@ -3,3 +3,11 @@ import ast
 tree = ast.parse("1 + 2")
 for node in ast.walk(tree):
     print(ast.dump(node))
+
+#Output:
+#Module(body=[Expr(value=BinOp(left=Num(n=1), op=Add(), right=Num(n=2)))])
+#Expr(value=BinOp(left=Num(n=1), op=Add(), right=Num(n=2)))
+#BinOp(left=Num(n=1), op=Add(), right=Num(n=2))
+#Num(n=1)
+#Add()
+#Num(n=2)
