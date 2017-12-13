91cfd1c786dfc7773222b1acc47252ab5b71fdb1
Erich Kauffman <kauffm2@stolaf.edu>
2017-12-13 14:37:28 -0600

    Create translator
    Create translator for binOps and assignments

--- examples/transArithOps.py
+++ examples/transArithOps.py
@@ -1,8 +1,6 @@
 import ast
 
-
 #Sturcture to store the datatypes for variables since the AST does not do this
-#may not be an appropriate solution when we start working on functions
 varTypeStore = {}
 
 #Set datatype of variable
@@ -14,7 +12,7 @@ def setType(var, varType):
 #at this point, this function should only be used with the ast.Assign datatype
 def getType(tree):
     if isinstance(tree, ast.Num):
-        return("int")
+        return("int ")
     elif isinstance(tree, ast.BinOp):
         return(getType(tree.left))
     elif isinstance(tree, ast.Name):
@@ -30,7 +28,7 @@ def translate(tree):
     if isinstance(tree, ast.Assign):
         varType = getType(tree.value)
         setType(tree.targets[0].id, varType)
-        stringTrans += varType + " " + translate(tree.targets[0]) + " = " + translate(tree.value)
+        stringTrans += varType + translate(tree.targets[0]) + " = " + translate(tree.value)
         return(stringTrans)
     elif isinstance(tree, ast.Num):
         return(str(tree.n))
@@ -49,30 +47,23 @@ def translate(tree):
         return(" / ")
     elif isinstance(tree, ast.Mod):
         return(" % ")
-    #TODO: still need these operators
+    #still need these operators
     #elif isinstance(tree, ast.FloorDiv) "//"
     #elif isinstance(tree, ast.Pow) "**"
     else:
         return stringTrans
 
 
-#Breaks down code block into lines and translates each line separately
 def translateCodeBlock(tree):
     for i in tree.body:
         print( "\t" + translate(i) + ";")
 
 
 #Fetch python code and create ast
-#TODO: Allow user to choose file
-tree = ast.parse(open("./examples/mockPy.py").read())
-
-#TODO: Write to file instead of printing to stdout
+tree = ast.parse(open("mockPy.py").read())
 print("#include <iostream>")
 print("using namespace std;")
 print("int main(){")
 translateCodeBlock(tree)
 print("\treturn 0;")
 print("}")
-
-
-#write to output file
