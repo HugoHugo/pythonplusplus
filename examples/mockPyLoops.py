edb52b0cac56fc268ac294595a0f37dc885160e8
Hugo Valent <valenthugo@hotmail.com>
2017-12-16 16:17:25 -0600

    basic for/while loop working

---
+++ examples/mockPyLoops.py
@@ -0,0 +1,4 @@
+#for p in range(0,10):
+#    p
+while(1>10):
+    3
--- pythonplusplus.py
+++ pythonplusplus.py
@@ -40,6 +40,27 @@ def getType(tree):
 #Depending on the datatype, translate returns a string corresponding to each node
 def translate(tree):
     stringTrans = ""
+    print(tree)
+
+    if isinstance(tree, ast.While):
+        final = "while("
+        final = final + translate(tree.test)
+        final = final + ") {\n"
+        final = final + translateCodeBlock(tree.body)
+        final = final + "   }"
+        return final
+
+
+    if isinstance(tree, ast.For): #a 'for in range' type loop
+        if tree.iter.func.id == "range":
+            final = "for(int i ="
+            v1 = translate(tree.iter.args[0])
+            v2 = translate(tree.iter.args[1])
+            final = final + v1 + ";" + " i < " + v2 + "; ++i) {\n"
+            final = final + translateCodeBlock(tree.body)
+            final = final + "\n" + "    }"
+            return final
+        return "Not defined"
 
     if isinstance(tree, ast.Try):
         final = "try{ \n"
@@ -61,7 +82,6 @@ def translate(tree):
         final = final + translateCodeBlock(tree.body)
         final = final + "    }"
         return final
-
     #variables
     if isinstance(tree, ast.Assign):
         varType = ""
@@ -171,17 +191,18 @@ def translateCodeBlock(tree):
 
 #Fetch python code and create ast
 #TODO: Allow user to choose file
-tree = ast.parse(open("./examples/mockPyExceptions.py").read())
+tree = ast.parse(open("./examples/mockPyLoops.py").read())
 
 #TODO: Write to file instead of printing to stdout
 print("#include <iostream>")
 print("using namespace std;")
 print("int main(){")
+print(ast.dump(tree))
 print(translateCodeBlock(tree.body))
 print("\treturn 0;")
 print("}")
 
-#Module(body=[Try(body=[Expr(value=BinOp(left=Num(n=2), op=Add(), right=Num(n=2)))], handlers=[ExceptHandler(type=None, name=None, body=[Expr(value=BinOp(left=Num(n=3), op=Add(), right=Num(n=3)))])], orelse=[], finalbody=[])])
+#Module(body=[While(test=Compare(left=Num(n=1), ops=[Gt()], comparators=[Num(n=10)]), body=[Expr(value=Num(n=3))], orelse=[])])
 #obj = []
 #for t in dir(tree):
 #    if not t.startswith('__' and '_'):
